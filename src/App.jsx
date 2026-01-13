a24ce10559f392b0a288dd07cef6535025b8dcb4
Tushar Rajpara <tusharrajpara00@gmail.com>
2026-01-13 07:52:31 +0530

    change and upload files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Pro1 from "./Lab10/Pro1";
+
+function App() {
+  return <>
+  <Pro1/>
+  </>;
+}
+
+export default App;
---
+++ src/Lab10/Pro1.jsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const Pro1 = () => {
+  return (
+    <div>
+        <Typography variant='p' sx={{color:"red",backgroundColor:"blue"}} component="h1">Hello world</Typography>
+    </div>
+  )
+}
+
+export default Pro1
